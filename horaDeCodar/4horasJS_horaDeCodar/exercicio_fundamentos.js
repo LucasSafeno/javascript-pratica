e1750a2d520b7cbaebeff904add9e9ce1a34878c
Lucas Safeno <lugnorio@gmail.com>
2023-08-27 12:59:30 -0300

    Curso JavaScript com exercicios - Hora de codar - #1:33:30

--- horaDeCodar/4horasJS_horaDeCodar/exercicio_fundamentos.js
+++ horaDeCodar/4horasJS_horaDeCodar/exercicio_fundamentos.js
@@ -28,3 +28,32 @@ for(let i = 0; i <= nomes.length; i++){
     console.log(nomes[i]);
 }
 
+
+// Exercucio 5: Funções, string e Math
+// Crie uma função que aceita uma string representando um horario no formato de 24 horas(exemplo 14:30). A função deve retornar uma retornar no formato de 12 horas (exemplo 2:30 PM).
+// use o objeto Math para auxiliar nessa conversão. Certifique-se de que a função lida corretamente com horários
+
+
+function conversaoHorario(horario24){
+    // 14:20 => hora 14, minuto 20 split(":")
+    // const hora = horario24.split(':')[0]
+    // const minuto = horario24.split(':')[1]
+
+    const [hora, minuto] = horario24.split(":");
+
+    const hora12 = hora  % 12 || 12;
+
+    let periodo  = "AM"
+
+    if(hora > 12){
+        periodo = "PM";
+    }
+
+    console.log(`${hora12}:${minuto} ${periodo}`);
+}
+
+conversaoHorario("12:54");
+
+conversaoHorario("16:54");
+
+conversaoHorario("8:54");
