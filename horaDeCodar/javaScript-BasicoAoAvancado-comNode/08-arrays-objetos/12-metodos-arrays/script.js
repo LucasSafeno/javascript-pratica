622846d96d6948c3d1daebb6bde08857b89402ae
Lucas Safeno <lugnorio@gmail.com>
2023-09-03 20:04:38 -0300

    08 - Arrays & Objetos - 12 Metodos de Arrays - pop e push

---
+++ horaDeCodar/javaScript-BasicoAoAvancado-comNode/08-arrays-objetos/12-metodos-arrays/script.js
@@ -0,0 +1,14 @@
+// push - inserir elemento do array (no fim do array)
+// pop - remove elemento do array (no fim do array)
+
+let nomes = ["Lucas", "Thauanna", "Eysis"]
+
+let elementoRemovido = nomes.pop()
+
+console.log(elementoRemovido)
+console.log(nomes)
+
+
+nomes.push("Mária")
+console.log(nomes)
+
