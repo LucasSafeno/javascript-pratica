e0f3bf1ef4d9e6b38705a9ae9c5f2d3c8fd8ad6c
Lucas Safeno <lugnorio@gmail.com>
2023-09-03 20:07:41 -0300

    08 - Arrays & Objetos - 13 Metodos de Arrays - shift e unshift

---
+++ horaDeCodar/javaScript-BasicoAoAvancado-comNode/08-arrays-objetos/13-metodos-arrays-shift-unshift/script.js
@@ -0,0 +1,14 @@
+// shift - inserir elemento do array (no inicio do array)
+// unshift - remove elemento do array (no inicio do array)
+
+let carros = ["BMW", "Audi", "VW", "Fiat"]
+
+let removerPrimeirocarro = carros.shift()
+
+console.log(removerPrimeirocarro)
+console.log(carros)
+
+carros.unshift("Ferrari")
+console.log(carros)
+
+
