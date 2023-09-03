dcf82bc250bc8cea7ac1bcb3b89245c0793e5edd
Lucas Safeno <lugnorio@gmail.com>
2023-09-03 20:41:08 -0300

    08 - Arrays & Objetos - 15 Metodos de Arrays - slice

---
+++ horaDeCodar/javaScript-BasicoAoAvancado-comNode/08-arrays-objetos/14-metodos-arrays-slice/script.js
@@ -0,0 +1,8 @@
+// slice - retorna um array  a partir de outro array
+// O array retornado é determinado pelos parametros que são os  indices de inicio e o fim do novo array
+
+let nums = [0,1,2,3,4,5,6,7,8,9];
+
+console.log(nums.slice(4,5))
+console.log(nums.slice(4,6))
+console.log(nums.slice(2))
