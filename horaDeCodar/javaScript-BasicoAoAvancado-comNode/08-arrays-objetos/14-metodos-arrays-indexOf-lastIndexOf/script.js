70fae317fce4f04760013f206a7f93d94d2d0dc5
Lucas Safeno <lugnorio@gmail.com>
2023-09-03 20:19:49 -0300

    08 - Arrays & Objetos - 14 Metodos de Arrays - indexOf & lastIndexOf

---
+++ horaDeCodar/javaScript-BasicoAoAvancado-comNode/08-arrays-objetos/14-metodos-arrays-indexOf-lastIndexOf/script.js
@@ -0,0 +1,7 @@
+// indexOf - encontra indice de um determinado elemento
+// lastIndexof - encontra o ultimo indice de um determinado elemento
+
+let nums = [5,6,2,4,9,6,2]
+
+console.log(nums.indexOf(2))
+console.log(nums.lastIndexOf(2))
