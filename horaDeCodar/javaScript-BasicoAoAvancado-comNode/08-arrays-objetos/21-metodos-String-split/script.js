6835b9a5ad0f6715349cff60c9447e759ba44484
Lucas Safeno <lugnorio@gmail.com>
2023-09-03 21:07:47 -0300

    08 - Arrays & Objetos - 21 Metodos de String - split

---
+++ horaDeCodar/javaScript-BasicoAoAvancado-comNode/08-arrays-objetos/21-metodos-String-split/script.js
@@ -0,0 +1,8 @@
+// split - divide uma string por um separador e retorna um array
+
+let frase = "Testando o metodo split"
+
+console.log(frase.split(" "))
+
+let frutas = "Banana;manã;uva;pera;morango"
+console.log(frutas.split(";"))
