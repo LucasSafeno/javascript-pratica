ab1fb202e42dbd08b12c1044f268e0f59e4a8ef0
Lucas Safeno <lugnorio@gmail.com>
2023-09-03 21:13:04 -0300

    08 - Arrays & Objetos - 22 Metodos de String - Join

---
+++ horaDeCodar/javaScript-BasicoAoAvancado-comNode/08-arrays-objetos/22-metodos-String-Join/script.js
@@ -0,0 +1,8 @@
+// join - Junta elementos de um array  em uma frase, por meio de um separador 
+let frase = "Testando o método split"
+
+let palavras = frase.split(" ")
+
+let novaFrase = palavras.join("@")
+
+console.log(novaFrase)
