28eec31c40eab066a2c787a165748ada3ee44893
Lucas Safeno <lugnorio@gmail.com>
2023-09-06 00:15:19 -0300

    Javascript do Básico ao Avançado (com node e projetos) - Conceito Orientação Objetos - Aula 01

---
+++ horaDeCodar/javaScript-BasicoAoAvancado-comNode/10-orientacao_objetos/01-aula/script.js
@@ -0,0 +1,11 @@
+let cachorro = {
+    latir: function(){
+        console.log("au au")
+    },
+    rosnar: function(){
+        console.log("grrr");
+    }
+}
+
+cachorro.latir();
+cachorro.rosnar();
